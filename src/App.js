edf2c41d97437434a00d7b3a9604e6aabc21f233
kesh <kdooleea@yahoo.ca>
2020-04-02 08:56:04 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+require("dotenv").config();
 
 function App() {
+  // API key from dotenv
+  const APP_KEY = process.env.REACT_APP_API_KEY;
+
+  // states
+  const [search, setSearch] = useState("meat");
+  const [data, setData] = useState([]);
+
+  // refetch data when rendering
+  // [] : fetch only once when refreshing page
+  useEffect(() => {
+    getRequest();
+  }, []);
+
+  const getRequest = async () => {
+    let url = `https://dictionaryapi.com/api/v3/references/thesaurus/json/${search}?key=${APP_KEY}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    setData([json]);
+    console.log(json);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"></div>
+
+      <ol>
+        {data.map(item => (
+          <li key={item[0].meta.id}>{item[0].meta.id}</li>
+        ))}
+      </ol>
+
+      <div>
+        {data.map(item => (
+          <p key={item[0].shortdef[0]}>{item[0].shortdef[0]}</p>
+        ))}
+      </div>
     </div>
   );
 }
